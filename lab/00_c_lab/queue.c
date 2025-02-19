17468eb83c77bd73094315c3a1095db703cbd4a4
TonyYin <Yuwendong.Yin@outlook.com>
2025-02-19 17:00:52 +0800

    Finish 00_C_Programming_Lab

--- lab/00_c_lab/queue.c
+++ lab/00_c_lab/queue.c
@@ -23,12 +23,24 @@
 queue_t *q_new()
 {
     /* Remember to handle the case if malloc returned NULL */
-    return NULL;
+    queue_t *q = malloc(sizeof(queue_t));
+    if(q == NULL) return NULL;
+    q->size = 0;
+    q->head = q->tail = NULL;
+    return q;
 }
 
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
+    if(q == NULL) return ;
+    list_ele_t *u = q->head;
+    while(u != NULL) {
+        list_ele_t *v = u->next;
+        free(u);
+        u = v;
+    }
+    free(q);
     /* Remember to free the queue structue and list elements */
 }
 
@@ -40,7 +52,16 @@ void q_free(queue_t *q)
 bool q_insert_head(queue_t *q, int v)
 {
     /* What should you do if the q is NULL? */
+    if(q == NULL) return false;
     /* What if malloc returned NULL? */
+    list_ele_t *u = malloc(sizeof(list_ele_t));
+    if(u == NULL) return false;
+    // 正常情况
+    q->size++;
+    u->value = v;
+    u->next = q->head;
+    q->head = u;
+    if(q->size == 1) q->tail = q->head;
     return true;
 }
 
@@ -53,7 +74,20 @@ bool q_insert_head(queue_t *q, int v)
 bool q_insert_tail(queue_t *q, int v)
 {
     /* Remember: It should operate in O(1) time */
-    return false;
+    if (q == NULL) return false;
+    list_ele_t *u = malloc(sizeof(list_ele_t));
+    if (u == NULL) return false;
+    // 正常情况
+    q->size++;
+    u->value = v;
+    u->next = NULL;
+    if(q->size == 1) { // 此时 q->tail->next == NULL
+        q->tail = q->head = u;
+    } else {
+        q->tail->next = u;
+        q->tail = u;
+    }
+    return true;
 }
 
 /*
@@ -65,6 +99,14 @@ bool q_insert_tail(queue_t *q, int v)
 */
 bool q_remove_head(queue_t *q, int *vp)
 {
+    if (q == NULL || !q->size) return false;
+    
+    list_ele_t *u = q->head;
+    q->head = q->head->next;
+    
+    if(vp != NULL) *vp = u->value;
+    free(u);
+    q->size--;
     return true;
 }
 
@@ -75,7 +117,8 @@ bool q_remove_head(queue_t *q, int *vp)
 int q_size(queue_t *q)
 {
     /* Remember: It should operate in O(1) time */
-    return 0;
+    if(q == NULL) return 0;
+    return q->size;
 }
 
 /*
@@ -87,6 +130,18 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-
+    if(q == NULL || q->size <= 1) return;
+    q->tail = q->head; // 用不上tail
+    // 滑动(x->y->z)，每次改成y->x
+    list_ele_t *x = q->head; 
+    list_ele_t *y = q->head->next;
+    q->head->next = NULL; // 避免成环
+    while(y != NULL) {
+        list_ele_t *z = y->next;
+        y->next = x;
+        x = y;
+        y = z;
+    }
+    q->head = x;
 }
 
